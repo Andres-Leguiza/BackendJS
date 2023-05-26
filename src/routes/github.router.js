948595d5935646928708e4903994cb74d88df280
strato_andres@hotmail.com <strato_andres@hotmail.com>
2023-05-26 19:10:49 -0300

    Entrega Final - Fixing UI issues.

--- src/routes/github.router.js
+++ src/routes/github.router.js
@@ -1,11 +1,11 @@
 import { Router } from 'express';
 import passport from '../utils/passport.util.js';
 import * as Constants from "../constants/constants.js";
-import * as GithubController from '../controllers/github.controller.js';
+import { handleCallback } from '../controllers/github.controller.js';
 
 const githubRouter = Router();
 
 githubRouter.get('/login', passport.authenticate("github", { scope:["user:email"] }));
-githubRouter.get('/callback', passport.authenticate('github', { failureRedirect: Constants.GITHUB_LOGIN_FAIL_VIEW }), GithubController.handleCallback);
+githubRouter.get('/callback', passport.authenticate('github', { failureRedirect: Constants.GITHUB_LOGIN_FAIL_VIEW }), handleCallback);
 
 export default githubRouter;
